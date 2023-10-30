b860e0b455abc787199beb0088536bb084e47b2b
mahernassri <maher.nassri@gmail.com>
2023-10-29 20:06:55 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner getInput = new Scanner(System.in);
+        System.out.println("Enter purchase price (per share): ");
+        BigDecimal purchasePrice = getInput.nextBigDecimal();
+
+        int days = 1;
+        BigDecimal closePrice = BigDecimal.valueOf(0.01);
+
+        while (true)
+        {
+            System.out.println("Enter the closing price for day: " + days + " (any negative value to exit");
+            closePrice = getInput.nextBigDecimal();
+
+            if (closePrice.compareTo(BigDecimal.ZERO) < 0) break;
+            BigDecimal earnings = closePrice.subtract(purchasePrice);
+
+            if (earnings.compareTo(BigDecimal.ZERO) > 0)
+            {
+                System.out.println("After day " + days + " , you earned $" + earnings + " per share.");
+            } else if (earnings.compareTo(BigDecimal.ZERO) < 0)
+            {
+                System.out.println("After day " + days + " , you lost $" + earnings + " per share.");
+            } else
+            {
+                System.out.println("After day " + days + ", you have broken even.");
+            }
+
+            days++;
+        }
+        getInput.close();
+    }
+}
