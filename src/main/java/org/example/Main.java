24a8dcad705607ba433978c336af1838e14c15b5
mahernassri <maher.nassri@gmail.com>
2023-10-31 19:23:54 -0400

    Updated to always show 2 decimal spaces.
    Updated to show profit/loss.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main
@@ -8,31 +9,41 @@ public class Main
     public static void main(String[] args)
     {
         Scanner getInput = new Scanner(System.in);
-        System.out.println("Enter purchase price (per share): ");
+        System.out.print("Enter purchase price (per share): ");
         BigDecimal purchasePrice = getInput.nextBigDecimal();
+        ArrayList<BigDecimal> priceList = new ArrayList<>();
+        priceList.add(purchasePrice);
 
         int days = 1;
         BigDecimal closePrice = BigDecimal.valueOf(0.01);
 
-        while (true)
+        while (closePrice.compareTo(BigDecimal.ZERO) > 0.00)
         {
-            System.out.println("Enter the closing price for day: " + days + " (any negative value to exit");
+            System.out.print("Enter the closing price for day: " + days + " (any negative value to exit: ");
             closePrice = getInput.nextBigDecimal();
 
-            if (closePrice.compareTo(BigDecimal.ZERO) < 0) break;
+            if (closePrice.compareTo(BigDecimal.ZERO) < 0)
+            {
+                BigDecimal profitLoss = priceList.get((priceList.size() - 1)).subtract(purchasePrice);
+                System.out.println("Final profit/loss is: $" + profitLoss.setScale(2, RoundingMode.HALF_UP));
+                System.out.println("Negative value detected.  Exiting...");
+                break;
+            }
             BigDecimal earnings = closePrice.subtract(purchasePrice);
 
             if (earnings.compareTo(BigDecimal.ZERO) > 0)
             {
                 System.out.println("After day " + days + " , you earned $" + earnings.setScale(2, RoundingMode.HALF_UP) + " per share.");
+                priceList.add(closePrice);
             } else if (earnings.compareTo(BigDecimal.ZERO) < 0)
             {
                 System.out.println("After day " + days + " , you lost $" + earnings.setScale(2, RoundingMode.HALF_UP) + " per share.");
+                priceList.add(closePrice);
             } else
             {
                 System.out.println("After day " + days + ", you have broken even.");
+                priceList.add(closePrice);
             }
-
             days++;
         }
         getInput.close();
